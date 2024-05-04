e437d22c602e05096a56bbf9aa4cd61352a2653f
Mohamed Khaled <mohammedref34t@gmail.com>
2024-05-04 17:19:25 +0300

    Careers Query

--- templates/careers_shortcode.php
+++ templates/careers_shortcode.php
@@ -1,4 +1,26 @@
 <div class="align-wide">
-   
-<h3><?= $title; ?></h3>
+<h3 style="margin-bottom:30px"><?= $title; ?></h3>
+
+<?php
+
+$args = array(
+    'post_type' => 'job',
+    'post_status' => 'publish',
+    'posts_per_page' => 10,
+);
+
+$query = new WP_Query( $args );
+
+if ($query->have_posts()) :
+    echo '<ul style="list-style:none; padding:0;">';
+    while ($query->have_posts()) : $query->the_post();
+        echo '<li style="margin-bottom:20px;border: 1px solid; padding: 10px;"><p>'.get_the_title().'</p><a href='. get_the_permalink() . '> Learn More</a></li>';
+    endwhile;
+    echo '</ul>';
+endif;
+
+wp_reset_postdata();
+
+?>
+
 </div>
